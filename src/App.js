f89df9ac02148f5bc8d504b88eb2f99f4c352d15
wakazoku <wakazoku@gmail.com>
2021-01-04 21:17:51 +0900

    複数の状態を管理する

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const double = () => setCount(count * 2);
-  const reset = () => setCount(0);
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-  const double2 = () => setCount((previousCount) => previousCount * 2);
-  const reset2 = () => setCount(() => 0);
-  const devide3 = () =>
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const reset = () => {
+    setName(props.name);
+    setPrice(props.price);
+  };
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-        <button onClick={double}>x2</button>
-        <button onClick={reset}>reset</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>+1</button>
-        <button onClick={double2}>x2</button>
-        <button onClick={reset2}>reset</button>
-      </div>
-      <div>
-        <button onClick={devide3}>3の倍数だけ3で割る</button>
-      </div>
+      <p>
+        現在の{name}は{price}円です
+      </p>
+      <button
+        onClick={() => {
+          setPrice(price + 1);
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          setPrice(price - 1);
+        }}
+      >
+        -1
+      </button>
+      <button onClick={reset}>reset</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "sample",
+  price: 1000,
 };
 
 export default App;
