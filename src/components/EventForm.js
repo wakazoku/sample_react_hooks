6271cbf993ee69a0e590b21779621e17fcd35a5c
wakazoku <wakazoku@gmail.com>
2021-01-09 19:35:06 +0900

    操作ログを保存する

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from "react";
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  // DELETE_OPERATION_LOGS,
+} from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils.js";
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -13,6 +19,11 @@ const EventForm = () => {
       type: CREATE_EVENT,
       title,
       body,
+    });
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: "イベントを作成しました",
+      operatedAt: timeCurrentIso8601(),
     });
     setTitle("");
     setBody("");
@@ -27,6 +38,13 @@ const EventForm = () => {
       dispatch({
         type: DELETE_ALL_EVENTS,
       });
+    if (result) {
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: "全てのイベントを削除しました",
+        operatedAt: timeCurrentIso8601(),
+      });
+    }
   };
 
   const unCreatable = title === "" || body === "";
