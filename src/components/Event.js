024058d6f0ae2101bb47ee2f651f86aa2acc8079
wakazoku <wakazoku@gmail.com>
2021-01-09 19:48:07 +0900

    イベント削除時の操作ログを実装する

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import AppContext from "../contexts/AppContext";
 
-import { DELETE_EVENT } from "../actions";
+import { DELETE_EVENT, ADD_OPERATION_LOG } from "../actions";
+import { timeCurrentIso8601 } from "../utils";
 
 const Event = ({ index, event }) => {
   const { dispatch } = useContext(AppContext);
@@ -11,7 +12,14 @@ const Event = ({ index, event }) => {
     const result = window.confirm(
       `イベント(id=${id})を本当に削除しても良いですか？`
     );
-    if (result) dispatch({ type: DELETE_EVENT, id });
+    if (result) {
+      dispatch({ type: DELETE_EVENT, id });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント(id=${id})を削除しました`,
+        operatedAt: timeCurrentIso8601(),
+      });
+    }
   };
 
   return (
