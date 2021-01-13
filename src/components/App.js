da1a11c3f9995aff0f88719c4cd027d6302a8643
wakazoku <wakazoku@gmail.com>
2021-01-13 20:53:40 +0900

    localstorageで永続化する

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import reducer from "../reducers";
 import AppContext from "../contexts/AppContext";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,12 +6,21 @@ import EventForm from "./EventForm";
 import Events from "./Events";
 import OperationLogs from "./OperationLogs";
 
+const APP_KEY = "appWithRedux";
+
 const App = () => {
-  const initialState = {
-    events: [],
-    operationLogs: [],
-  };
+  const appState = localStorage.getItem(APP_KEY);
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     <AppContext.Provider
